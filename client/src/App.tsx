94942016f14a58bc0203b3151367c63673ee15c8
Amir <sagan.5050@gmail.com>
2023-10-11 05:11:46 +0300

    add types/peerjs and adding server.ts for peer confg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
+import {Peer} from "peerjs"
 
 function App() {
+  const [peer, setPeer] = useState<Peer>();
+  const [mainStream, setMainStream] = useState<MediaStream>();
+
+  useEffect(() => {
+    const peer = new Peer({
+      host: 'localhost:3000',
+      path: '/peerjs'
+    });
+
+    setPeer(peer);
+
+    peer.on('open', (id) => {
+      console.log(id);
+    })
+
+    return () => {
+      peer.destroy();
+    }
+  })
+
 
   return (
     <>
-      
+      <video id="main-stream" autoPlay ></video>
     </>
   )
 }
