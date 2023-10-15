4bee7efe6dcec4ab368d3b9229789a57e27afb78
Amir <sagan.5050@gmail.com>
2023-10-15 07:59:20 +0300

    add react Router and socket.io

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Room from './Room.tsx'
+
+const BrowserRouter = createBrowserRouter([
+  {
+    path: "/",
+    action: () => {
+      return <App />
+    }
+  },
+  {
+    path: "/:roomid",
+    action: () => {
+      return <Room/>
+    }
+  }
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={BrowserRouter}>
   </React.StrictMode>,
 )
