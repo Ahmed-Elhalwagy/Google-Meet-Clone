11f4ca434cd494e13c54aeeb58f688333940207b
Amir <amir-elsagan@outlook.com>
2023-10-19 19:58:09 +0300

    cmplete mainvideo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,49 +4,12 @@ import {Peer} from "peerjs"
 import ChatBox from "./components/ChatBox";
 
 function App() {
-  const [peer, setPeer] = useState<Peer>();
-  const [mainStream, setMainStream] = useState<MediaStream>();
-  const [peerId, setPeerId] = useState<string>('');
-  const MainStream =   useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    const peer = new Peer({
-      host: 'localhost:3000',
-      path: '/peerjs'
-    });
-
-    setPeer(peer);
-
-    peer.on('open', (id) => {
-      console.log('My peer ID is: ' + id);
-      setPeerId(id);
-    })
-    console.log(peer.id);
-    return () => {
-      peer.destroy();
-    }
-  },[]);
-
-  const startStream = () => {
-    navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true
-    }).then((stream) => {
-      setMainStream(stream);
-      MainStream.current!.srcObject = stream;
-    }).then(()=> {
-      
-    }).catch((err) => {
-      console.log(err);
-    })
-  }
-
-0
+  
 
   return (
     <>
-      <video id="main-stream" autoPlay ref={MainStream} ></video>
-      <button onClick={startStream} id="start-stream">Start Stream</button>
+      
+      <button id="start-stream">Start Stream</button>
       <ChatBox/>
     </>
   )
---
+++ client/src/components/MainVideo.tsx
@@ -0,0 +1,93 @@
+import  { useEffect, useRef, useState } from 'react'
+import Peer, { MediaConnection } from 'peerjs'
+import io from 'socket.io-client'
+
+const getCurrentRoomID = () => {
+  const path = window.location.pathname.split('/');
+  if(path[path.length-1]==='')
+    return path[path.length-2];
+  else 
+    return path[path.length-1];
+}
+
+type Peers = {
+  [key: string]: MediaConnection;
+};
+
+export default function MainVideo() {
+  const [myPeer,] = useState(new Peer({
+    port: 5000,
+    host: 'localhost',
+    path: '/peerjs',
+  }));
+  const [peers, setPeers] = useState({} as Peers);
+  const [ROOM_ID,] = useState(getCurrentRoomID()); //window.location.pathname.split('/')[2
+  const [socket,] = useState(io("http://localhost:5000"));
+  // const [stream, setStream] = useState(null)
+  const myVideo = useRef(document.getElementById('main-video') as HTMLVideoElement);
+  const videoGrid = useRef(document.getElementById('video-grid') as HTMLDivElement);
+
+  useEffect(() => {
+    
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(stream => {
+      addVideoStream(myVideo.current, stream)
+    
+      myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+          addVideoStream(video, userVideoStream)
+        })
+      })
+    
+      socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+      })
+    })
+
+    socket.on('user-disconnected', userId => {
+      if (peers[userId]) peers[userId].close()
+    })
+    
+    myPeer.on('open', id => {
+      socket.emit('join-room', ROOM_ID, id)
+    })
+    return ()=> {
+      myPeer.destroy()
+    }
+  }, []);
+
+  function connectToNewUser(userId: string, stream: MediaStream) {
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+      video.remove()
+    })
+  
+    setPeers(prev => {
+      return {...prev, [userId]: call}
+    })
+  }
+  
+  function addVideoStream(video:HTMLVideoElement , stream: MediaStream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+      video.play()
+    })
+    videoGrid.current.append(video)
+  }
+
+
+  return (
+    <div>
+      <video id="main-video" ref={myVideo} className="video-player" autoPlay playsInline></video>
+      <div id="video-grid" ref={videoGrid}></div>
+    </div>
+  )
+}
